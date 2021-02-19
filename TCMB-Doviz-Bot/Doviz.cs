f9ee17ae72ffb8d9f5ae7c9a1544f9e8c543b3de
TurkoBey <78680709+TurkoBey@users.noreply.github.com>
2021-02-19 19:11:33 +0300

    Initial commit

---
+++ TCMB-Doviz-Bot/Doviz.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCMB_Doviz_Bot
+{
+	class Doviz
+	{
+		public string KurADI { get; set; }
+		public string TurkceKurADI { get; set; }
+		public string AlisFiyati { get; set; }
+		public string SatisFiyati { get; set; }
+		public string EfektikAlis { get; set; }
+		public string EfektifSatis { get; set; }
+	}
+}
---
+++ TCMB-Doviz-Bot/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TCMB_Doviz_Bot
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			TcmbKurCek();
+			Console.ReadLine();
+		}
+
+		private static void TcmbKurCek()
+		{
+			try
+			{
+				List<Doviz> DovizListesi = new List<Doviz>();
+				XmlDocument doc = new XmlDocument();
+				doc.Load("https://www.tcmb.gov.tr/kurlar/today.xml");
+				XmlNodeList DovizPath = doc.SelectNodes("/Tarih_Date/Currency");
+
+				foreach (XmlNode Doviz in DovizPath)
+				{
+					string TurkishName = Doviz.SelectSingleNode("Isim").InnerText;
+					string CurrencyName = Doviz.SelectSingleNode("CurrencyName").InnerText;
+					string ForexBuying = Doviz.SelectSingleNode("ForexBuying").InnerText;
+					string ForexSelling = Doviz.SelectSingleNode("ForexSelling").InnerText;
+					string BanknoteBuying = Doviz.SelectSingleNode("BanknoteBuying").InnerText;
+					string BanknoteSelling = Doviz.SelectSingleNode("BanknoteSelling").InnerText;
+
+					DovizListesi.Add(new Doviz()
+					{
+						TurkceKurADI = TurkishName,
+						KurADI = CurrencyName,
+						AlisFiyati = ForexBuying,
+						SatisFiyati = ForexSelling,
+						EfektikAlis = BanknoteBuying,
+						EfektifSatis = BanknoteSelling
+					});
+				}
+				Console.WriteLine("======================");
+				foreach (var Doviz in DovizListesi)
+				{
+					Console.WriteLine($"Döviz Cinsi        :: {Doviz.TurkceKurADI}" + " / " + $"{Doviz.KurADI}");
+					Console.WriteLine($"Döviz Alış         :: {Doviz.AlisFiyati}");
+					Console.WriteLine($"Döviz Satış        :: {Doviz.SatisFiyati}");
+					Console.WriteLine($"Banka Aliş Fiyati  :: {Doviz.EfektikAlis}");
+					Console.WriteLine($"Banka Satiş Fiyati :: {Doviz.EfektifSatis}");
+					Console.WriteLine("======================");
+				}
+			}
+			catch (Exception msjver)
+			{
+				Console.WriteLine(msjver.Message);
+			}
+		}
+	}
+}
